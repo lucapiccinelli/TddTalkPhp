4f94e7910b31b5879f168b83637e7974013bea67
Luca Piccinelli <luca.picci@gmail.com>
2023-03-08 21:59:18 +0100

    repo init e test list

---
+++ tests/Kata/EmployeesLoaderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kata;
+
+use PHPUnit\Framework\TestCase;
+
+// Happy path
+// File non esistente
+// Formato errato del file
+
+use DateTime;
+
+class EmployeesLoaderTest extends TestCase
+{
+    public function testCanLoadEmployeesFromFile() {
+    }
+}

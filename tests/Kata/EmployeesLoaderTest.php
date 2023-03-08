46fb4ab5ff28b47f9f006148e93bf6ad8584c920
Luca Piccinelli <luca.picci@gmail.com>
2023-03-08 22:00:14 +0100

    test does not compile

--- tests/Kata/EmployeesLoaderTest.php
+++ tests/Kata/EmployeesLoaderTest.php
@@ -4,14 +4,19 @@ namespace Kata;
 
 use PHPUnit\Framework\TestCase;
 
-// Happy path
-// File non esistente
-// Formato errato del file
-
 use DateTime;
 
 class EmployeesLoaderTest extends TestCase
 {
     public function testCanLoadEmployeesFromFile() {
+        $filename = 'Resources/employees.txt';
+        $employees = EmployeesLoader::Load($filename);
+
+        $expectedEmployees = array(
+            new Employee('John', 'Doe', DateTime::createFromFormat('Y-m-d', '1982-10-08'), 'john.doe@foobar.com'),
+            new Employee('Mary', 'Ann', DateTime::createFromFormat('Y-m-d', '1975-09-11'), 'mary.ann@foobar.com')
+        );
+
+        $this->assertEquals($expectedEmployees, $employees);
     }
 }
